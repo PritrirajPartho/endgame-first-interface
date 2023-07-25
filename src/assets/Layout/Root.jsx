619252277abf7e0397c45fbd0cb0f19716ef0b84
PritrirajPartho <dapartho5616@gmail.com>
2023-07-25 23:20:20 +0600

    mid-level

---
+++ src/assets/Layout/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from '../components/Home/Home/Home';
+
+const Root = () => {
+    return (
+        <div className='text-emerald-600'>
+            <Home></Home>
+        </div>
+    );
+};
+
+export default Root;
